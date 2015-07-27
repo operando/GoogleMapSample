c3098443f10bd66c9067beb572719ab9fb42e17c
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-07-27 23:13:34 +0900

    Show Google Map

---
+++ GoogleMapSample/GoogleMapSample-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  GoogleMapSample-Bridging-Header.h
+//  GoogleMapSample
+//
+//  Created by shinobu okano on 2015/07/27.
+//  Copyright (c) 2015年 shinobu okano. All rights reserved.
+//
+
+#ifndef GoogleMapSample_GoogleMapSample_Bridging_Header_h
+#define GoogleMapSample_GoogleMapSample_Bridging_Header_h
+
+#import <GoogleMaps/GoogleMaps.h>
+#import <Keys/GooglemapsampleKeys.h>
+
+#endif
---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+pod 'GoogleMaps'
+
+plugin 'cocoapods-keys', {
+    :project => "GoogleMapSample",
+    :keys => [
+    "GoogleApiKey"
+]}
