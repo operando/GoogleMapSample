2373c0b81e5e3f0cae87feb76e74596aa58958e5
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-08-02 16:48:53 +0900

    Change Setting

--- GoogleMapSample/ViewController.swift
+++ GoogleMapSample/ViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 6)
+        // 35.681382,139.766084
+        var camera = GMSCameraPosition.cameraWithLatitude(35.681382,
+            longitude: 139.766084, zoom: 15)
         var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         mapView.myLocationEnabled = true
+//         mapView.accessibilityElementsHidden = false
+//         mapView.indoorEnabled = true
+//         mapView.mapType = kGMSTypeHybrid
+//        var mapInsets = UIEdgeInsetsMake(50.0, 0.0, 0.0, 100.0)
+//        mapView.padding = mapInsets
+//        mapView.settings.zoomGestures = false
+//        mapView.settings.compassButton = true
+        mapView.settings.myLocationButton = true
         self.view = mapView
+        mapView.delegate = self
         
         var marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2DMake(35.681382, 139.766084)
+        marker.title = "Tokyo Station"
+        marker.snippet = "Tokyo"
         marker.map = mapView
     }
     
@@ -30,6 +40,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func mapView(mapView: GMSMapView!, willMove gesture: Bool) {
+        NSLog("will move")
+    }
 }
 
